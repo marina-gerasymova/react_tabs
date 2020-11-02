07e00d990adb0fff0d5ebfb7439c4a8c324a5a09
Marina <m.gerasymova29@gmail.com>
2020-11-02 18:49:08 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cb55ce10f2eb2a4d10df16c6247cd130.js"
+  "./precache-manifest.d7b959309c17d09e42eb203fdcbe01db.js"
 );
 
 self.addEventListener('message', (event) => {
